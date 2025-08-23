be386a877184cd8bcbb9d6c224531e9f95d19768
Kay Zin Khaing <kayzinkhaing1331@gmail.com>
2025-08-23 08:32:32 +0630

    update and modify design

--- assets/js/script.js
+++ assets/js/script.js
@@ -95,9 +95,9 @@ document.addEventListener('visibilitychange',
 var typed = new Typed(".typing-text", {
     strings: ["frontend development", "backend development", "web designing", "web development" , "full stack development"],
     loop: true,
-    typeSpeed: 50,
-    backSpeed: 25,
-    backDelay: 500,
+    typeSpeed: 80,
+    backSpeed: 50,
+    backDelay: 800,
 });
 // <!-- typed js effect ends -->
 
